df751da5788354347c3cf1a1679ae2d3c2f3e871
Daniel F <daniel.yopla@team.wrike.com>
2023-09-11 21:27:03 +0200

    Improving logger

--- Sources/Lumberjack/Lumberjack.swift
+++ Sources/Lumberjack/Lumberjack.swift
@@ -1,38 +1,82 @@
 import Foundation
 import OSLog
 
-public protocol Logging {
-    func debug(_ message: OSLogMessage)
-    func info(_ message: OSLogMessage)
-    func warning(_ message: OSLogMessage)
-    func error(_ message: OSLogMessage)
+public enum LogLevel {
+    case debug
+    case info
+    case warning
+    case error
+
+    var icon: String {
+        switch self {
+        case .debug:
+            return "🥋"
+        case .info:
+            return "🔉"
+        case .warning:
+            return "⚠️"
+        case .error:
+            return "🚨"
+        }
+    }
 }
 
-public struct LumberjackCoordinator: Logging {
+public struct LogInfo {
+    let logLevel: LogLevel
+    let message: String
+
+    var formattedMessage: String {
+        return "\(logLevel.icon) - \(message)"
+    }
+
+    public init(logLevel: LogLevel, message: String) {
+        self.logLevel = logLevel
+        self.message = message
+    }
+}
+
+public protocol Logging {
+    func log(logInfo: LogInfo)
+}
+
+public struct LumberjackCoordinator {
     var loggers: [any Logging]
 
     public init(loggers: [any Logging]) {
         self.loggers = loggers
     }
 
-    public func debug(_ message: OSLogMessage) {
-        loggers.forEach { $0.debug(message) }
+    public func debug(_ message: String) {
+        loggers.forEach { $0.log(logInfo: .init(logLevel: .debug, message: message)) }
     }
 
-    public func info(_ message: OSLogMessage) {
-        loggers.forEach { $0.info(message) }
-    }
-    
-    public func warning(_ message: OSLogMessage) {
-        loggers.forEach { $0.warning(message) }
+    public func info(_ message: String) {
+        loggers.forEach { $0.log(logInfo: .init(logLevel: .info, message: message)) }
     }
 
-    public func error(_ message: OSLogMessage) {
-        loggers.forEach { $0.error(message) }
+    public func warning(_ message: String) {
+        loggers.forEach { $0.log(logInfo: .init(logLevel: .warning, message: message)) }
+    }
+
+    public func error(_ message: String) {
+        loggers.forEach { $0.log(logInfo: .init(logLevel: .error, message: message)) }
     }
 }
 
 extension Logger: Logging {
+    public func log(logInfo: LogInfo) {
+        switch logInfo.logLevel {
+        case .debug:
+            return self.debug("\(logInfo.formattedMessage)")
+        case .info:
+            return self.info("\(logInfo.formattedMessage)")
+        case .warning:
+            return self.warning("\(logInfo.formattedMessage)")
+        case .error:
+            return self.error("\(logInfo.formattedMessage)")
+        }
+    }
+
     private static var subsystem = Bundle.main.bundleIdentifier!
     public static let main = Logger(subsystem: subsystem, category: "main")
 }
